5b692595ef6c85a9c757b0c19895b8706d8bcf70
gosrunga <gsrungarapu@qvantel.com>
2018-05-24 18:32:21 +0530

    Updating to pythonanywhere

--- fakelinkshub/urls.py
+++ fakelinkshub/urls.py
@@ -3,6 +3,11 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='index'),
+    # url(r'^$', views.index, name='index'),
+    url(r'^$', views.signIn, name='index'),
+    url(r'^postsign/', views.postsign),
+    url(r'^logout/', views.logout, name="logout"),
+    url(r'^signup/', views.signUp, name="signup"),
+    url(r'^postsignup/', views.postsignup),
     url(r'^submit_url$', views.submit_url, name='submit'),
 ]
